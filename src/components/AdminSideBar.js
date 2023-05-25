c9c9c02c1c214c15860620cca03b2bf62a08d544
GilJuLee <dlrlfwn95@gmail.com>
2023-05-25 10:22:15 +0900

    fix: adminsidebar 구현중 누락부분 수정

--- src/components/AdminSideBar.js
+++ src/components/AdminSideBar.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "../css/admin.css";
 
 const AdminSideBar = () => {
-    const [customerclick, setCustomerclick] = useState(false);
+
     const navigate = useNavigate();
 
     const freelancerclick = () => {
@@ -16,16 +16,14 @@ const AdminSideBar = () => {
     const projectclick = () => {
         navigate('/admin/project')
     };
-    const handlecustomerclick = () => {
-        setCustomerclick(!customerclick);
-    };
+
     return (
         <div className="sidebar-admin">
             <h3>관리자님, 환영합니다</h3>
             <h4 onClick={freelancerclick}>유저 리스트</h4>
             <h4 onClick={clientclick}>클라이언트 리스트</h4>
             <h4 onClick={projectclick}>프로젝트 리스트</h4>
-            <h4 onClick={handlecustomerclick}>고객센터 리스트</h4>
+            <h4>고객센터 리스트</h4>
             <div>
                 <p>
                     <Link to="/admin/sca">┖ 답변</Link>
